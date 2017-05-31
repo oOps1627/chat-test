e4b9f6219119207277fa5f52dc8d624741a3fe7d
oOps1627 <andriy162726@gmail.com>
2017-05-31 23:40:11 +0300

    added mongo db

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||'mongodb://localhost/chat');
+mongoose.connect('mongodb://andriy1627:f162726@ds157278.mlab.com:57278/chat', process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||'mongodb://localhost/chat');
 
 module.exports = mongoose;
