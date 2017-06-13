50b2ff829f540bcbe5777c3a9b0fcda3de64b710
oOps1627 <andriy162726@gmail.com>
2017-06-13 13:22:26 +0300

    final fixed db

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/chat');
-
+mongoose.connect('mongodb://oops:162726@ds157278.mlab.com:57278/chat');
 module.exports = mongoose;
 
-// 'mongodb://oops:162726@ds157278.mlab.com:57278/chat' ||
+// 'mongodb://oops:162726@ds157278.mlab.com:57278/chat'
+// 'mongodb://localhost/chat'
