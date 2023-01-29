5f5ec1fbaa827e4a79357cbd1459dbe4336d02eb
Андрій Крашівський <a.krashivskyi@avidi.tech>
2023-01-29 17:51:37 +0200

    fix dependencies in package-json.json

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://oops:162726@ds157278.mlab.com:57278/chat');
+mongoose.connect('mongodb://localhost:27017');
 module.exports = mongoose;
 
 // 'mongodb://oops:162726@ds157278.mlab.com:57278/chat'
